a8157bf75e16cbbbc1299f8c14f3dea23585d163
trahe19 <trahe2000@gmail.com>
2026-02-25 12:10:31 -0500

    Change animation class for logo from fadeUp to logoZoom

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function HomePage() {
         width={650}
         height={250}
         priority
-        className="mb-14 drop-shadow-[0_0_70px_rgba(255,255,255,0.9)] animate-fadeUp"
+        className="mb-14 drop-shadow-[0_0_70px_rgba(255,255,255,0.9)] animate-logoZoom"
       />
 
       {/* Headline */}
