9882075b798881712625b1d20e321b2a659cb105
trahe19 <trahe2000@gmail.com>
2026-02-26 13:32:45 -0500

    Update page.tsx

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -1,15 +1,28 @@
+'use client'
+
+import Script from "next/script";
+
 export default function JoinPage() {
   return (
     <div className="min-h-screen bg-black flex items-center justify-center px-4 py-10">
-      <div className="w-full max-w-4xl bg-white rounded-xl overflow-hidden">
-        <iframe
-          src="https://docs.google.com/forms/d/e/1FAIpQLSeTiiUzHzSGWoaTR-GMbZsa1gYLL175yGDEGDMXCcCs78Elvg/viewform?embedded=true"
-          width="100%"
-          height="1000"
-          style={{ border: "none" }}
-          title="FlicksPicks Waitlist Form"
+
+      <div className="w-full max-w-4xl bg-white rounded-xl p-4">
+
+        <div
+          style={{ width: "100%", height: "600px" }}
+          data-fillout-id="vMjS7i6SjCus"
+          data-fillout-embed-type="standard"
+          data-fillout-inherit-parameters
+          data-fillout-dynamic-resize
+        ></div>
+
+        <Script
+          src="https://server.fillout.com/embed/v1/"
+          strategy="afterInteractive"
         />
+
       </div>
+
     </div>
   );
 }
