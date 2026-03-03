369bbffc85b9d660ae9f695a745d6a6867ba74ab
trahe19 <trahe2000@gmail.com>
2026-03-03 09:41:32 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function HomePage() {
       <div className="background-light light-right" />
 
       {/* ===== MAIN CONTENT ===== */}
-     <main className="relative z-10 min-h-screen flex flex-col items-center justify-center px-6 px-y-24 text-center text-white">
+   <main className="relative z-10 min-h-screen flex flex-col items-center justify-start px-6 pt-32 pb-40 text-center text-white">
 
         <div className="curtain-left" />
         <div className="curtain-right" />
@@ -58,10 +58,12 @@ export default function HomePage() {
           Join the Waitlist
         </a>
 {/* Trailer Section */}
-       <h2 className={`text-3xl md:text-4xl mb-6 uppercase tracking-widest ${orbitron.className}`}>
-  Watch The Trailer
-</h2>
-<div className="w-full max-w-3xl mt-32 px-6">
+<div className="w-full max-w-3xl mt-40 px-6">
+
+  <h2 className={`text-3xl md:text-4xl mb-8 uppercase tracking-widest ${orbitron.className}`}>
+    Watch The Trailer
+  </h2>
+
   <div className="relative w-full pb-[56.25%] rounded-2xl overflow-hidden shadow-2xl">
     <iframe
       className="absolute top-0 left-0 w-full h-full"
@@ -71,6 +73,7 @@ export default function HomePage() {
       allowFullScreen
     ></iframe>
   </div>
+
 </div>
       </main>
     </>
