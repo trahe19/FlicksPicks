890ae64bc7cd9b4d000aa1baf78214078b7842aa
trahe19 <trahe2000@gmail.com>
2026-03-05 11:34:28 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,29 +13,28 @@ const oswald = Oswald({
 
 export const metadata = {
   title: "FlicksPicks",
-  description: "The World's First Fantasy Film League Platform.",
+  description: "The Fantasy Film League Platform",
 
   icons: {
-    icon: [
-      { url: "/fp-icon.png", sizes: "32x32", type: "image/png" },
-      { url: "/fp-icon.png", sizes: "192x192", type: "image/png" },
-    ],
-    apple: "/apple-touch-icon.png",
+    icon: "/fp-icon.png",
   },
 
   openGraph: {
     title: "FlicksPicks",
-    description: "The World's First Fantasy Film League Platform.",
+    description: "The Fantasy Film League Platform",
     images: [
       {
-        url: "/og-image.png",
+        url: "https://flickspicks.com/og-image.png",
         width: 1200,
         height: 630,
-        alt: "FlicksPicks",
       },
     ],
   },
 
+  twitter: {
+    card: "summary_large_image",
+    images: ["https://flickspicks.com/og-image.png"],
+  },
 }
 
 export default function RootLayout({
